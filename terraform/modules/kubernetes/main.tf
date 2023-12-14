d2686b8bef178f81f060ba45ac8b9887cdbd7057
JonathanManass <jonathan.manassen@discngine.com>
2023-12-14 09:19:23 +0000

    add echo to tdecision resource deletion

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -873,6 +873,8 @@ resource "helm_release" "tdecision_chart" {
       kubectl delete -n ${self.namespace} job --all --force
       kubectl delete deployments -n ${self.namespace} --all --force
       kubectl delete pods -n ${self.namespace} --all --force
+      kubectl get all -n ${self.namespace}
+      echo "finished deleting resources"
     EOT
   }
 }
