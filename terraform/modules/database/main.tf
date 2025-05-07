c9359857f5754e6216b8f936d89549545b2623f1
JonathanManass <jonathan.manassen@discngine.com>
2025-05-07 15:31:41 +0000

    fix: correct indentation for apply_immediately and option_group_name in db_instance resource

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -22,6 +22,7 @@ resource "aws_security_group_rule" "db_security_group_egress" {
 }
 
 resource "aws_db_instance" "db_instance" {
+  apply_immediately        = true
   max_allocated_storage    = 1000
   character_set_name       = "AL32UTF8"
   instance_class           = var.instance_type
@@ -30,7 +31,7 @@ resource "aws_db_instance" "db_instance" {
   parameter_group_name     = aws_db_parameter_group.db_param_group.name
   engine                   = "oracle-se2-cdb"
   license_model            = var.license_type
-option_group_name          = var.db_migration ? aws_db_option_group.s3_option_group[0].name : "default:oracle-se2-cdb-19"
+  option_group_name        = var.db_migration ? aws_db_option_group.s3_option_group[0].name : "default:oracle-se2-cdb-19"
   port                     = "1521"
   multi_az                 = var.high_availability
   db_subnet_group_name     = aws_db_subnet_group.subnet_group.name
