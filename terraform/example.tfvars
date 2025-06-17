4d6a482dacd0804e2eb29916eac1c6e8b4c88791
JonathanManass <85951131+JonathanManass@users.noreply.github.com>
2025-06-17 18:27:20 +0200

    Postgres (#65)
    * Add postgres helm chart & remove choral / chembl charts
    
    * change to newest scripts for postgres
    
    * Add kubernetes reflector helm chart configuration
    
    * Add CHEMBL_29 database configuration and update Jenkins pipeline for production
    
    * Update external secret namespace selector to include postgres
    
    * Add Kubernetes secret for PostgreSQL connection string
    
    * Update database connection string and enhance security context for bingo-fetch container
    
    * Refactor Terraform configuration for improved readability and add Azure OIDC certificate handling
    
    * Refactor OIDC variable definitions to use optional object types for improved flexibility and clarity
    
    * Fix SQL table creation to use correct alias for compact molecule in STRUCTURE_SMALL_MOL
    
    * Fix index creation in PostgreSQL setup and update Helm release values format to YAML
    
    * Fix formatting issue in initial_db_passwords for CHEMBL_29 in CloudFormation template
    
    * Update CHANGELOG.md for version 3.4.2: Add support for EKS Kubernetes versions 1.30–1.32, enhance Terraform modules, and improve resource handling
    
    * Update CHANGELOG.md and reduce externalSecrets refresh intervals to 1 second for improved performance
    
    * Update refresh time for external secrets to 1 minute for improved stability
    
    * Update package.zip with latest function changes
    
    * Increase timeout for secret rotator lambda function from 30 to 120 seconds to account for 1 minute sleep before unlock
    
    * Update CHANGELOG.md for version 3.4.2: Refine EKS Kubernetes version support, enhance Terraform module details, and improve formatting consistency
    
    * Update tdecision_chart version to 3.4.2
    
    ---------
    
    Co-authored-by: aphilippejolivel <axel.philippe-jolivel@discngine.com>

--- terraform/example.tfvars
+++ terraform/example.tfvars
@@ -9,12 +9,12 @@ force_destroy = false
 keypair_name = null
 
 # Whether to create snapshots of public & private volumes when deleting them (can take up to 1h)
-public_final_snapshot = true
+public_final_snapshot  = true
 private_final_snapshot = true
 
 # If set to false, give vpc_id and private_subnet_ids. Otherwise leave those blank
-create_network = false
-vpc_id = ""
+create_network     = false
+vpc_id             = ""
 private_subnet_ids = []
 
 # Custom ami to use for EKS, if left null the default eks ami will be used
@@ -36,9 +36,9 @@ load_balancer_type = "internal"
 certificate_arn = ""
 
 # Domain information
-domain = 
+domain         = "yourdomain.com"
 main_subdomain = "3decision"
-api_subdomain = "3decision-api"
+api_subdomain  = "3decision-api"
 
 # ROUTE 53 Hosted zone id
 hosted_zone_id = null
@@ -65,13 +65,13 @@ google_oidc = {
 }
 
 # Used for disaster recovery, ids of the public & private volumes to recreate from
-public_volume_snapshot = null
+public_volume_snapshot  = null
 private_volume_snapshot = null
 
 # Can generally be left as is
 
 kubernetes_version = "1.27"
-db_instance_type = "db.t3.xlarge"
-license_type = "license-included"
-eks_instance_type = "t3.2xlarge"
-boot_volume_size = "50"
+db_instance_type   = "db.t3.xlarge"
+license_type       = "license-included"
+eks_instance_type  = "t3.2xlarge"
+boot_volume_size   = "50"
--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -102,14 +102,6 @@ resource "kubernetes_namespace" "redis_namespace" {
   depends_on = [kubernetes_config_map_v1.aws_auth]
 }
 
-resource "kubernetes_namespace" "choral_namespace" {
-  metadata {
-    name = "choral"
-  }
-
-  depends_on = [kubernetes_config_map_v1.aws_auth]
-}
-
 resource "kubernetes_namespace" "tools_namespace" {
   metadata {
     name = "tools"
@@ -250,10 +242,10 @@ spec:
   externalSecretName: database-secrets
   namespaceSelector:
     matchExpressions:
-      - {key: kubernetes.io/metadata.name, operator: In, values: [${var.tdecision_chart.namespace}, choral, tools]}
+      - {key: kubernetes.io/metadata.name, operator: In, values: [${var.tdecision_chart.namespace}, tools, postgres]}
   refreshTime: 1m
   externalSecretSpec:
-    refreshInterval: 1m
+    refreshInterval: 1s
     secretStoreRef:
       name: aws-secret-store
       kind: ClusterSecretStore
@@ -277,15 +269,10 @@ spec:
       remoteRef:
         key: 3dec-chembl_29-db
         property: password
-    - secretKey: CHORAL_DB_PASSWD
-      remoteRef:
-        key: 3dec-choral_owner-db
-        property: password
   YAML
   depends_on = [
     kubectl_manifest.secretstore,
     kubernetes_namespace.tdecision_namespace,
-    kubernetes_namespace.choral_namespace,
     kubernetes_config_map_v1.aws_auth
   ]
 }
@@ -297,12 +284,14 @@ resource "kubernetes_secret" "nest_authentication_secrets" {
   }
   data = merge(
     {
-      AZURE_TENANT        = var.azure_oidc.tenant
-      AZURE_SECRET        = var.azure_oidc.secret
-      GOOGLE_SECRET       = var.google_oidc.secret
-      OKTA_DOMAIN         = var.okta_oidc.domain
-      OKTA_SERVER_ID      = var.okta_oidc.server_id
-      OKTA_SECRET         = var.okta_oidc.secret
+      AZURE_TENANT                 = var.azure_oidc.tenant
+      AZURE_SECRET                 = var.azure_oidc.secret
+      AZURE_CERTIFICATE_THUMBPRINT = var.azure_oidc.certificate_thumbprint
+      AZURE_CERTIFICATE_KEY        = var.azure_oidc.certificate_key_path
+      GOOGLE_SECRET                = var.google_oidc.secret
+      OKTA_DOMAIN                  = var.okta_oidc.domain
+      OKTA_SERVER_ID               = var.okta_oidc.server_id
+      OKTA_SECRET                  = var.okta_oidc.secret
     },
     var.pingid_oidc.client_id == "none" ? {} : {
       PINGID_SECRET       = var.pingid_oidc.secret
@@ -310,6 +299,16 @@ resource "kubernetes_secret" "nest_authentication_secrets" {
     }
   )
   depends_on = [kubernetes_namespace.tdecision_namespace, kubernetes_config_map_v1.aws_auth]
+}
+
+resource "kubernetes_secret" "azure_certificate_key" {
+  metadata {
+    name      = "certificate-key"
+    namespace = "tdecision"
+  }
+  data = {
+    "key.pem" = "${var.azure_oidc.certificate_key}"
+  }
 }
 
 resource "kubernetes_job_v1" "af_bucket_files_push" {
@@ -694,7 +693,7 @@ aws_destroy_resources: ${null_resource.delete_resources.id}
 YAML
 }
 
-# This resets the passwords of schemas CHEMBL_29 and PD_T1_DNG_THREEDECISION
+# This resets the passwords of schema PD_T1_DNG_THREEDECISION
 # It is aimed to be used on new environments that have unknown passwords set in the backup
 resource "terraform_data" "reset_passwords" {
   count = var.initial_db_passwords["ADMIN"] != "Ch4ng3m3f0rs3cur3p4ss" ? 1 : 0
@@ -728,7 +727,6 @@ spec:
         - echo "resetting passwords";
           echo -ne "ALTER USER CHEMBL_29 IDENTIFIED BY \"\$${CHEMBL_DB_PASSWD}\" ACCOUNT UNLOCK;
           ALTER USER PD_T1_DNG_THREEDECISION IDENTIFIED BY \"\$${DB_PASSWD}\" ACCOUNT UNLOCK;
-          ALTER USER CHORAL_OWNER IDENTIFIED BY \"\$${CHORAL_DB_PASSWD}\" ACCOUNT UNLOCK;" > reset_passwords.sql;
           exit | /home/sqlcl/sqlcl/bin/sql ADMIN/\$${SYS_DB_PASSWD}@\$${CONNECTION_STRING} @reset_passwords.sql;
 YAML
 kubectl delete -f reset_passwords.yaml
@@ -738,65 +736,6 @@ kubectl delete -f reset_passwords.yaml
 kubectl apply -f reset_passwords.yaml
 rm reset_passwords.yaml
     EOF
-  }
-  depends_on = [kubectl_manifest.ClusterExternalSecret]
-}
-
-# This deletes both the CHORAL_OWNER user and choral indexes
-# It is aimed to be used on new environments that would retrieve this data from a backup when it has to be installed
-resource "terraform_data" "clean_choral" {
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = <<EOF
-aws eks update-kubeconfig --name EKS-tdecision --kubeconfig $HOME/.kube/config
-export KUBECONFIG=$HOME/.kube/config
-if kubectl get deploy -n choral | grep choral &> /dev/null; then
-  echo "choral already running, cleaning aborted."
-  exit 0
-fi
-cat > clean_choral.yaml << YAML
----
-apiVersion: v1
-kind: Pod
-metadata:
-  name: clean-choral
-  namespace: choral
-spec:
-  restartPolicy: Never
-  containers:
-    - name: clean-choral
-      image: fra.ocir.io/discngine1/prod/oracle/sqlcl:23.4.0.023.2321
-      command: [ "/bin/bash", "-c", "--" ]
-      envFrom:
-      - secretRef:
-          name: database-secrets
-      env:
-        - name: CONNECTION_STRING
-          value: ${local.connection_string}
-      args:
-        - echo 'dropping chembl indexes';
-          echo -ne 'DROP INDEX IDX_CHOR_STR_CMP_STRUC FORCE;
-          DROP INDEX IDX_CHOR_TAU_CMP_STRUC FORCE;
-          DROP INDEX IDX_CHOR_TAUISO_CMP_STRUC FORCE;
-          DROP INDEX IDX_CHOR_STRICT_CMP_STRUC FORCE;' > drop_chembl_index.sql;
-          exit | /home/sqlcl/sqlcl/bin/sql CHEMBL_29/\$${CHEMBL_DB_PASSWD}@\$${CONNECTION_STRING} @drop_chembl_index.sql;
-          echo 'dropping tdec indexes';
-          echo -ne 'DROP INDEX IDX_CHOR_STR_SMALL_MOL_SMILES FORCE;
-          DROP INDEX IDX_CHOR_TAU_SMALL_MOL_SMILES FORCE;
-          DROP INDEX IDX_CHOR_TAUISO_SMS FORCE;
-          DROP INDEX IDX_CHOR_STRISO_SMS FORCE;' > drop_t1_index.sql;
-          exit | /home/sqlcl/sqlcl/bin/sql PD_T1_DNG_THREEDECISION/\$${DB_PASSWD}@\$${CONNECTION_STRING} @drop_t1_index.sql;
-          echo 'dropping choral owner schema';
-          echo -ne 'DROP USER CHORAL_OWNER CASCADE;' > drop_choral_owner.sql;
-          exit | /home/sqlcl/sqlcl/bin/sql ADMIN/\$${SYS_DB_PASSWD}@\$${CONNECTION_STRING} @drop_choral_owner.sql
-YAML
-kubectl delete -n choral pod/clean-choral
-kubectl apply -f clean_choral.yaml
-rm clean_choral.yaml
-    EOF
-  }
-  lifecycle {
-    ignore_changes = all
   }
   depends_on = [kubectl_manifest.ClusterExternalSecret]
 }
@@ -858,39 +797,192 @@ resource "null_resource" "delete_resources" {
   }
 }
 
-resource "helm_release" "choral_chart" {
-  name      = var.choral_chart.name
-  chart     = var.choral_chart.chart
-  version   = var.choral_chart.version
-  namespace = var.choral_chart.namespace
-  values = [<<YAML
-${var.disable_choral_dns_resolution ? "exposeHostName: false" : ""}
-oracle:
-  connectionString: ${local.connection_string}
-pvc:
-  storageClassName: ${local.storage_class}
-  YAML
-  ]
-  timeout = 1200
-  depends_on = [
-    kubernetes_storage_class_v1.encrypted_storage_class,
-    kubectl_manifest.ClusterExternalSecret,
-    helm_release.aws_load_balancer_controller,
-    kubernetes_config_map_v1.aws_auth,
-    terraform_data.clean_choral,
-  ]
-  lifecycle {
-    ignore_changes = all
+resource "kubernetes_secret" "connection_string_postgres" {
+  metadata {
+    name      = "connection-string"
+    namespace = "postgres"
+  }
+  data = {
+    CONNECTION_STRING = local.connection_string
   }
 }
 
-resource "helm_release" "chemaxon_ms_chart" {
-  name             = var.chemaxon_ms_chart.name
-  chart            = var.chemaxon_ms_chart.chart
-  namespace        = var.chemaxon_ms_chart.namespace
-  create_namespace = var.chemaxon_ms_chart.create_namespace
-  version          = var.chemaxon_ms_chart.version
+resource "helm_release" "postgres_chart" {
+  name             = var.postgres_chart.name
+  chart            = var.postgres_chart.chart
+  namespace        = var.postgres_chart.namespace
+  create_namespace = var.postgres_chart.create_namespace
+  version          = var.postgres_chart.version
   timeout          = 1200
+
+  values = [
+    <<YAML
+image:
+  tag: 14.18.0
+secretAnnotations:
+  reflector.v1.k8s.emberstack.com/reflection-allowed: "true"
+  reflector.v1.k8s.emberstack.com/reflection-auto-enabled: "true"
+primary:
+  extendedConfiguration: |-
+    shared_buffers = 500MB
+  extraEnvVars:
+    - name: "POSTGRESQL_STATEMENT_TIMEOUT"
+      value: "300000"
+  resources:
+    limits:
+      memory: 1Gi
+    requests:
+      memory: 1Gi
+      cpu: 200m
+  persistence:
+    enabled: false
+  extraVolumeMounts:
+    - name: export-volume
+      mountPath: /export
+    - name: bingo-volume
+      mountPath: /bingo
+
+  initContainers:
+    - name: export-from-oracle
+      image: fra.ocir.io/discngine1/oracle/instantclient:23
+      command:
+        - /bin/bash
+        - -c
+        - |
+            set -e
+            sqlplus -s PD_T1_DNG_THREEDECISION/$${DB_PASSWD}@$${CONNECTION_STRING} <<SQL > /dev/null
+            SET HEADING OFF
+            SET FEEDBACK OFF
+            SET PAGESIZE 0
+            SET LINESIZE 10000
+            SET TRIMSPOOL ON
+            SET TERMOUT OFF
+            SET ECHO OFF
+            SET COLSEP ','
+            SPOOL export.csv
+            SELECT SMALL_MOL_ID || ',' || SMILES FROM STRUCTURE_SMALL_MOL;
+            SPOOL OFF
+            EXIT
+            SQL
+            sed '1d;$d' export.csv > /export/export.csv
+
+      volumeMounts:
+        - name: export-volume
+          mountPath: /export
+      envFrom:
+        - secretRef:
+            name: database-secrets
+        - secretRef:
+            name: connection-string
+    - name: bingo-fetch
+      image: fra.ocir.io/discngine1/prod/alpine/wget-unzip-tar:3.22.0
+      command:
+        - sh
+        - -c
+        - |
+          set -e
+          mkdir -p /bingo
+          wget "https://lifescience.opensource.epam.com/downloads/bingo-1.29.0/bingo-postgres-14-linux-x86_64.zip" -O /bingo/bingo.zip
+          cd /bingo
+          unzip -o bingo.zip
+          tar -xzf bingo*.tgz
+      securityContext:
+        seccompProfile:
+          type: RuntimeDefault
+        allowPrivilegeEscalation: false
+        runAsNonRoot: true
+        runAsUser: 1001
+        runAsGroup: 1001
+      volumeMounts:
+        - name: bingo-volume
+          mountPath: /bingo
+
+  sidecars:
+    - name: post-init
+      image: bitnami/postgresql:14.18.0
+      command:
+        - /bin/bash
+        - -c
+        - |
+          until pg_isready -h localhost -U $POSTGRES_USER -d $POSTGRES_DB; do sleep 1; done
+          echo "Waiting for Postgres to listen on 0.0.0.0:5432..."
+          while ! grep -q '00000000:1538' /proc/net/tcp; do
+            sleep 1
+          done
+
+          sleep 10
+          until pg_isready -h localhost -U $POSTGRES_USER -d $POSTGRES_DB; do sleep 1; done
+          echo "Running POST-INIT Script..."
+          PGPASSWORD="$POSTGRES_PASSWORD" psql -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB" << 'EOF'
+          SET statement_timeout = 0;
+          create index index_bingo_mol on structure_small_mol using bingo_idx (smiles bingo.bmolecule);
+          ANALYZE;
+          EOF
+
+          echo "Done. Idling to avoid restart..."
+          sleep infinity
+      env:
+        - name: POSTGRES_USER
+          value: postgres
+        - name: POSTGRES_DB
+          value: postgres
+        - name: POSTGRES_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: postgresql
+              key: postgres-password
+  initdb:
+    scripts:
+      bingo.sh: |
+        #!/bin/bash
+        set -e
+
+        echo "Starting bingo init script at $(date)"
+
+        start_time=$(date +%s)
+
+        bingo_dir=$(find /bingo/ -maxdepth 1 -type d -name 'bingo*' ! -path /bingo/ | head -n1)
+        cd "$bingo_dir"
+        chmod +x bingo-pg-install.sh
+        echo -e "y\n" | ./bingo-pg-install.sh
+        PGPASSWORD="$${POSTGRESQL_PASSWORD}" psql -U "$${POSTGRESQL_USERNAME}" -d "$${POSTGRESQL_DATABASE}" -f bingo_install.sql
+        PGPASSWORD="$${POSTGRESQL_PASSWORD}" psql -U "$${POSTGRESQL_USERNAME}" -d "$${POSTGRESQL_DATABASE}" <<'EOF'
+
+        CREATE TABLE TEMP_STRUCTURE_SMALL_MOL (
+          small_mol_id INTEGER NOT NULL,
+          smiles TEXT
+        );
+        \copy temp_structure_small_mol(small_mol_id, smiles) FROM '/export/export.csv' DELIMITER ',' CSV
+        create table STRUCTURE_SMALL_MOL as (select small_mol_id, bingo.compactmolecule(SMILES, false) as smiles from temp_structure_small_mol where bingo.CheckMolecule(SMILES) is null);
+
+        UPDATE bingo.bingo_config
+        SET cvalue = '300000'
+        WHERE cname = 'TIMEOUT';
+        EOF
+        end_time=$(date +%s)
+        duration=$(( end_time - start_time ))
+
+        echo "Finished bingo init script at $(date)"
+        echo "Total duration: $${duration} seconds"
+
+  extraVolumes:
+    - name: export-volume
+      emptyDir: {}
+    - name: bingo-volume
+      emptyDir: {}
+YAML
+  ]
+  depends_on = [kubernetes_secret.connection_string_postgres]
+}
+
+resource "helm_release" "kubernetes_reflector" {
+
+  name             = var.kubernetes_reflector_chart.name
+  chart            = var.kubernetes_reflector_chart.chart_name
+  repository       = var.kubernetes_reflector_chart.repository
+  version          = var.kubernetes_reflector_chart.version
+  namespace        = var.kubernetes_reflector_chart.namespace
+  create_namespace = var.kubernetes_reflector_chart.create_namespace
 }
 
 locals {
