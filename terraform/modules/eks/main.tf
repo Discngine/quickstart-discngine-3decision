94c034e623a5b378b07a4786f63e6fb827bd8ff0
JonathanManass <jonathan.manassen@discngine.com>
2025-10-17 15:50:07 +0000

    Update EKS AMI path and refactor userdata configuration for custom and default AMIs

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -144,12 +144,14 @@ EOF
 data "aws_ssm_parameter" "eks_ami_release_version" {
   count = var.custom_ami == "" ? 1 : 0
 
-  name = "/aws/service/eks/optimized-ami/${local.cluster.version}/amazon-linux-2023/recommended/image_id"
+  name = "/aws/service/eks/optimized-ami/${local.cluster.version}/amazon-linux-2023/x86_64/standard/recommended/image_id"
 }
 
 # AL2023 nodeadm configuration for maxPods
-data "cloudinit_config" "eks_node_userdata" {
-  count = var.create_node_group && var.user_data == "" ? 1 : 0
+# For managed node groups with custom AMI, we need full cluster config
+# For managed node groups with default AMI, we only configure kubelet
+data "cloudinit_config" "eks_node_userdata_custom_ami" {
+  count = var.create_node_group && var.user_data == "" && var.custom_ami != "" ? 1 : 0
 
   base64_encode = true
   gzip          = false
@@ -166,6 +168,29 @@ spec:
     name: ${local.cluster.name}
     apiServerEndpoint: ${local.cluster.endpoint}
     certificateAuthority: ${local.cluster.certificate_authority[0].data}
+  kubelet:
+    config:
+      maxPods: 110
+    EOT
+  }
+}
+
+# For managed node groups with default AMI, only specify kubelet config
+# EKS will automatically merge cluster connection details
+data "cloudinit_config" "eks_node_userdata_default_ami" {
+  count = var.create_node_group && var.user_data == "" && var.custom_ami == "" ? 1 : 0
+
+  base64_encode = true
+  gzip          = false
+  boundary      = "MIMEBOUNDARY"
+
+  part {
+    content_type = "application/node.eks.aws"
+    content      = <<-EOT
+---
+apiVersion: node.eks.aws/v1alpha1
+kind: NodeConfig
+spec:
   kubelet:
     config:
       maxPods: 110
@@ -195,8 +220,14 @@ resource "aws_launch_template" "EKSLaunchTemplate" {
     associate_public_ip_address = false
   }
 
-  # Use cloudinit config for AL2023 with maxPods, or custom user_data if provided
-  user_data = var.user_data != "" ? base64encode(var.user_data) : try(data.cloudinit_config.eks_node_userdata[0].rendered, null)
+  # Use appropriate cloudinit config based on whether custom AMI is used
+  user_data = var.user_data != "" ? base64encode(var.user_data) : try(
+    coalesce(
+      try(data.cloudinit_config.eks_node_userdata_custom_ami[0].rendered, null),
+      try(data.cloudinit_config.eks_node_userdata_default_ami[0].rendered, null)
+    ),
+    null
+  )
 
   metadata_options {
     http_put_response_hop_limit = 2
