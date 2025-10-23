f7359c3a60c5ed42195b89afbd2520fe2e36e834
JonathanManass <jonathan.manassen@discngine.com>
2025-10-23 13:58:31 +0000

    Update default monitoring email for ALB health alerts to ensure notifications are sent

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -565,6 +565,6 @@ variable "enable_alb_monitoring" {
 
 variable "monitoring_email" {
   type        = string
-  default     = ""
+  default     = "cloudsquad@discngine.com"
   description = "Email address to receive ALB health monitoring alerts. Leave empty to disable email notifications."
 }
