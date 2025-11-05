412798817424b27dcb8f99cda827a1441faad5ad
JonathanManass <jonathan.manassen@discngine.com>
2025-11-05 18:00:37 +0000

    Remove lifecycle block from helm_release resource to allow change tracking

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -778,9 +778,6 @@ resource "helm_release" "tdecision_chart" {
     terraform_data.reset_passwords,
     helm_release.postgres_chart
   ]
-  lifecycle {
-    ignore_changes = all
-  }
 
   provisioner "local-exec" {
     when    = destroy
