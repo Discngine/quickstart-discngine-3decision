8d41e70c1f2f10c25bf701be7392313a99f3b0dd
aphilippejolivel <axel.philippe-jolivel@discngine.com>
2026-03-30 07:58:48 +0000

    Update CI configuration and adjust database storage parameters

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -244,13 +244,13 @@ variable "skip_db_final_snapshot" {
 }
 
 variable "db_allocated_storage" {
-  default     = 900
+  default     = 650
   type        = number
   description = "Initial allocated storage in GB. When restoring from snapshot, RDS uses the larger of this value or the snapshot size. Set high enough to avoid immediate autoscaling and the 6-hour cooldown (e.g. 500 for data migration)."
 }
 
 variable "max_allocated_storage" {
-  default     = 1000
+  default     = 1500
   type        = number
   description = "Maximum autoscaled size of the database"
 }
@@ -343,7 +343,7 @@ variable "tdecision_chart" {
     name             = optional(string, "tdecision")
     chart            = optional(string, "oci://fra.ocir.io/discngine1/prod/helm/tdecision")
     namespace        = optional(string, "tdecision")
-    version          = optional(string, "3.5.1")
+    version          = optional(string, "3.5.12")
     create_namespace = optional(bool, true)
   })
   default = {}
